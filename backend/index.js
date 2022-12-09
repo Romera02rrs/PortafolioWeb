ee8e62889aedd07c8ea40e590ea0ad61c84533d7
Romera02rrs <Romera02rrs@gmail.com>
2022-12-09 09:17:07 +0100

    Deploy test heroku

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,13 @@ const whiteList = [process.env.FRONTEND_URL]
 
 const corsOptions = {
     origin: function(origin, callback) {
+
+        console.log(origin);
+        console.log(whiteList);
+        
         if(whiteList.includes(origin)){
             callback(null, true)
         }else {
-            console.log(whiteList);
             callback(new Error("Error de Cors"))
         }
     }
